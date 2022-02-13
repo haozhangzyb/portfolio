214c1a324bea02ebe979f0b34707a8b7c0a56a7f
HaoOneBin <37146238+HaoOneBin@users.noreply.github.com>
2022-02-12 20:15:01 -0600

    done without responsive avatar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import * as React from "react";
+import { Paper, Typography, Grid, Avatar, Stack } from "@mui/material";
+import {
+  createTheme,
+  ThemeProvider,
+  responsiveFontSizes,
+  styled,
+} from "@mui/material/styles";
+import { makeStyles } from "@mui/styles";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
+import bgImg from "./bgImg.jpg";
+import avatar from "./avatar.jpg";
+
+const githubURL = "https://github.com/haozhangzyb";
+const linkedinURL = "https://www.linkedin.com/in/haozhangzyb/";
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: "#cfd8dc",
+    },
+    secondary: {
+      main: "#b0bec5",
+    },
+  },
+  spacing: 14,
+});
+
+theme = responsiveFontSizes(theme);
+
+// const useStyles = makeStyles((theme) => ({
+//   sizeAvatar: {
+//     height: theme.spacing(4),
+//     width: theme.spacing(4),
+//   },
+// }));
 
 function App() {
+  // const classes = useStyles(theme);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <React.Fragment>
+      <ThemeProvider theme={theme}>
+        <Grid
+          container
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          style={{
+            height: "100vh",
+            backgroundImage: `url(${bgImg})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+          }}
+          sx={{ pb: 20 }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Grid item xs={3} sx={{ mx: 1 }}>
+            <Avatar
+              alt="Hao Zhang"
+              src={avatar}
+              sx={{ width: 110, height: "auto", mx: "auto", mb: 1 }}
+            />
+            <Typography
+              component="h4"
+              variant="h4"
+              align="center"
+              color="primary"
+              gutterBottom
+            >
+              Hao Zhang
+            </Typography>
+            <Typography
+              variant="h6"
+              align="center"
+              color="secondary"
+              component="p"
+            >
+              MSCS @ Northwestern University
+            </Typography>
+            <Stack
+              direction="row"
+              align="center"
+              justifyContent="center"
+              spacing={2}
+              sx={{ mx: "auto", my: 1 }}
+            >
+              <a href={githubURL}>
+                <GitHubIcon color="secondary" />
+              </a>
+              <a href={linkedinURL}>
+                <LinkedInIcon color="secondary" />
+              </a>
+            </Stack>
+          </Grid>
+          {/* <Paper sx={{ height: "35vh" }}></Paper> */}
+        </Grid>
+      </ThemeProvider>
+    </React.Fragment>
   );
 }
 
